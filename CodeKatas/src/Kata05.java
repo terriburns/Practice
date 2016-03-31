753d347d2c0d303d3ee5b20b52475bbe70207f85
Terri Burns <terri.burns@nyu.edu>
2016-03-31 17:17:29 -0400

    Import all da classes

--- CodeKatas/src/Kata05.java
+++ CodeKatas/src/Kata05.java
@@ -1,10 +1,12 @@
+import java.util.*;
+
 /*Implement a Bloom filter based spell checker*/
 public class Kata05 {
-	public static void main(String[] args) {
-	  //step one: initialize a hashtable using the java class
-          File f = new File(kata5wordlist.txt);
-          long length = f.length();
-          Hashtable table = new Hashtable(length);
-        }
+  public static void main(String[] args) {
+    //step one: initialize a hashtable using the java class
+    File f = new File(kata5wordlist.txt);
+    long length = f.length();
+    Hashtable table = new Hashtable(length);
+  }
 
 }
